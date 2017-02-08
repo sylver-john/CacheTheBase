a57c98ee0738eb7c93d16bd781635aae3a493e23
Sylver Imhoff <Sylver-John.IMHOFF.ext@boursorama.fr>
2017-02-08 17:05:10 +0100

    api

--- api.go
+++ api.go
@@ -1,5 +1,49 @@
 package main
 
+import (
+	"log"
+	"net/http"
+    _ "github.com/go-sql-driver/mysql"
+    "database/sql"
+    "encoding/json"
+)
+
+type Data struct {
+	Id int
+	Text string
+}
+
 func main() {
 	// here we use the cache file generated instead of doing a sql request
+	http.HandleFunc("/", getData)
+	log.Print("launch the server")
+	if err := http.ListenAndServe(":1234", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }
+
+func getData(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("mysql","root:@tcp(127.0.0.1:3306)/base_test")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	rows, err := db.Query("SELECT * FROM info")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var data []Data
+    for rows.Next() {
+    	row := new(Data)
+        err = rows.Scan(&row.Id, &row.Text)
+		if err != nil {
+			log.Fatal(err)
+		}
+		data = append(data, *row)
+    }
+	json.NewEncoder(w).Encode(data)
+}
+
--- fileGenerator.go
+++ fileGenerator.go
@@ -1,5 +1,48 @@
 package main
 
+import(
+	"log"
+    _ "github.com/go-sql-driver/mysql"
+    "database/sql"
+    "os"
+    "io"
+)
+
+type Data struct {
+	Id int
+	Text string
+}
+
 func main() {
 	// here we make the sql request and put the result in a file
+	db, err := sql.Open("mysql","root:@tcp(127.0.0.1:3306)/base_test")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	rows, err := db.Query("SELECT * FROM info")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var data []Data
+    for rows.Next() {
+    	row := new(Data)
+        err = rows.Scan(&row.Id, &row.Text)
+		if err != nil {
+			log.Fatal(err)
+		}
+		data = append(data, *row)
+    }
+    file, err := os.Create("./cache/cache")
+    if err != nil {
+        log.Fatal(err)
+    }
+    _, err = io.Copy(file, data)
+    if err != nil {
+        log.Fatal(err)
+    }
+    file.Close()
 }
